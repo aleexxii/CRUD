e80f0ccd85ca5ae45d0904be4bc9598d764a0fca
Rahul <amaleexxii@gmail.com>
2024-01-20 18:14:59 +0530

    CRUD

---
+++ crud_app/server/services/render.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+
+exports.homeRoutes = (req,res)=>{
+    // make a get request to api users
+    axios.get("http://localhost:3000/api/users")
+    .then(function(response){
+        // console.log(response.data);
+        res.render('index',{users :response.data});
+    })
+    .catch(err => {
+        res.send(err);
+    })
+    
+}
+
+exports.add_user = (req,res) => {
+    res.render('user')
+}
+
+exports.update_user = (req,res)=>{
+    axios.get("http://localhost:3000/api/users",{params : {id : req.query.id}}) //i need a specific user
+    .then(function(userdata){
+        // console.log(userdata.data);
+        res.render("update_user",{user:userdata.data})
+    })
+    .catch(err => {
+        res.send(err)
+    })
+}
